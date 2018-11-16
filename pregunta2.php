5f34c1a826635b60338bda55d4ee23f2ed272f81
AnalystRoyal <analystroyalis@gmail.com>
2018-11-16 15:49:30 -0600

    Prototipo V1.0.0

---
+++ pregunta2.php
@@ -0,0 +1,41 @@
+	<!DOCTYPE html>
+	<html>
+	<head>
+		<title>pregunta1</title>
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous>
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	</head>
+
+	<body>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+	    <a class="navbar-brand" href="#Inicio">PROTOTIPO FORMULARIO</a>
+	    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+	      <li class="nav-item active">
+	        <a class="nav-link" href="#Cajero">Cajero <span class="sr-only">(current)</span></a>
+	      </li>
+	      <li class="nav-item active">
+	        <a class="nav-link" href="#Encuesta">Encuesta <span class="sr-only">(current)</span></a>
+	      </li>
+	      <li class="nav-item active">
+	        <a class="nav-link" href="#Consulta">Consulta <span class="sr-only">(current)</span></a>
+	      </li>
+	    </ul>   
+	  </div>
+	</nav>
+
+	<div class="card">
+	  <div class="card-body">
+	    <p>2. Esta es la pregunta 2</p>
+	    <a href="pregunta1.php"><button type="button" class="btn btn-primary btn-lg">Anterior</button></a>
+	    <a href="index.php"><button type="button" class="btn btn-primary btn-lg">Siguiente</button></a>
+	  </div>
+	</div>	
+		</div>
+	</body>
+	</html>
